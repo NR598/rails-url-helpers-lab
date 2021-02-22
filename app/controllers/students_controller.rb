0ace46b83cede72b5c44a01c67c88ffd2e0f0ba1
NR598 <norm.rich@gmail.com>
2021-02-22 03:11:39 -0500

    commit tree 8ed0dd5f864cfb7b278b71a2659f6120887d0a43

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,5 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
   def index
     @students = Student.all
   end
@@ -8,9 +7,15 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    @student = Student.find(params[:id])
+    @student.toggle!(:active)
+    redirect_to student_path(params[:id])
+  end
+
   private
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  def set_student
+    @student = Student.find(params[:id])
+  end
 end
